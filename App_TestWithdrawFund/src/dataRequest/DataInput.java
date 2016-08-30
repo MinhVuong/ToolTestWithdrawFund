1d563dff2814dc266fc914b3032ff2958d474c1a
MinhVuong <1212527@student.hcmus.edu.vn>
2016-08-30 16:52:49 +0700

    Start Again

---
+++ App_TestWithdrawFund/src/dataRequest/DataInput.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataRequest;
+
+/**
+ *
+ * @author CPU01661-local
+ */
+public class DataInput {
+    private String type;
+    private String name;
+
+    public DataInput() {
+    }
+
+    public DataInput(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ App_TestWithdrawFund/src/dataRequest/DataInputLevel2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataRequest;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CPU01661-local
+ */
+public class DataInputLevel2 {
+    private ArrayList<DataInput> listDataIputLevel2;
+
+    public DataInputLevel2() {
+    }
+
+    public DataInputLevel2(ArrayList<DataInput> listDataIputLevel2) {
+        this.listDataIputLevel2 = listDataIputLevel2;
+    }
+
+    public ArrayList<DataInput> getListDataIputLevel2() {
+        return listDataIputLevel2;
+    }
+
+    public void setListDataIputLevel2(ArrayList<DataInput> listDataIputLevel2) {
+        this.listDataIputLevel2 = listDataIputLevel2;
+    }
+    
+}
